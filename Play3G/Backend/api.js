a1b387c85d0eee86a70e7816ddab4c4c933ad275
Ollichka <salnikova.olia@gmail.com>
2016-03-17 15:57:24 +0200

    first step

---
+++ Play3G/Backend/api.js
@@ -0,0 +1,15 @@
+ 
+var : level_List = require('./data/Level_List');
+
+exports.getPizzaList = function(req, res) {
+    res.send(Pizza_List);
+};
+
+exports.createOrder = function(req, res) {
+    var order_info = req.body;
+    console.log("Creating Order", order_info);
+
+    res.send({
+        success: true
+    });
+};
---
+++ Play3G/Backend/data/Level_List.js
@@ -0,0 +1,9 @@
+/**
+ * Created by diana on 12.01.16.
+ */
+
+var level_info = [
+    { }
+];
+
+module.exports = level_info;
